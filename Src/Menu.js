453e25cfb0764bc41d90aac2b229db71592aa1af
Dennis Moiseyev <dmoiseye@ucsc.edu>
2020-04-23 12:34:45 -0700

    Menu Screen Updated

--- Src/Menu.js
+++ Src/Menu.js
@@ -9,11 +9,14 @@ class Menu extends Phaser.Scene {
     preload(){
 
         this.load.image("nightroad", "./Assets/NightRoadTS.png");
+        this.load.image("moon", "./Assets/Moon.png");
     }
 
     create(){
 
         this.backdrop= this.add.tileSprite(0, 0, 1500, 1700, "nightroad");
+
+        this.add.image(660, 100, "moon");
 
         this.add.text(450, 480, "A Heart-Shaped Box", {fontFamily: "Times New Roman", fontSize: "52px", color: "#FF0000"}).setOrigin(0.5);
     }
