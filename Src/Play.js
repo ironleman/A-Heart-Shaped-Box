4e5397e3c7160e3559b63aa9edc56803492cafb3
Dennis Moiseyev <dmoiseye@ucsc.edu>
2020-04-23 14:48:59 -0700

    Initiated Play Scene

--- Src/Play.js
+++ Src/Play.js
@@ -4,14 +4,20 @@ class Play extends Phaser.Scene {
     }
 
     preload(){
+        this.load.image("nightroad", "./Assets/NightRoad.png");
 
     }
 
     create(){
 
+        this.road= this.add.tileSprite(0, 0, 1500, 1700, "nightroad");
+
+
     }
 
     update(){
+
+        this.road.tilePositionX -=6;
 
     }
 
