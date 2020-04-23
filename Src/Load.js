04e0cde89b53f11a6b5db220931dbb5ade8d4403
Dennis Moiseyev <dmoiseye@ucsc.edu>
2020-04-23 15:34:34 -0700

    Started Load

--- Src/Load.js
+++ Src/Load.js
@@ -4,15 +4,13 @@ class Load extends Phaser.Scene {
     }
 
     preload(){
-        this.load.video("timelapse", "Timelapse.mp4", "loadeddata", false, true);
+        
 
     }
 
     create(){
 
-        this.vid= this.add.video(centerX, centerY, "timelapse");
-        this.vid.play(true);
-        //this.scene.start("menuScene");
+        
 
 
 
