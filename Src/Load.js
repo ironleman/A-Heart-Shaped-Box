56071cad5a5df67a903e970cb84fa9f714681db8
Dennis Moiseyev <dmoiseye@ucsc.edu>
2020-04-23 20:15:58 -0700

    Updated Menu

--- Src/Load.js
+++ Src/Load.js
@@ -9,8 +9,8 @@ class Load extends Phaser.Scene {
     }
 
     create(){
-
         
+        this.scene.start("menuScene");
 
 
 
--- Src/Menu.js
+++ Src/Menu.js
@@ -20,6 +20,8 @@ class Menu extends Phaser.Scene {
 
         this.add.text(450, 480, "A Heart-Shaped Box", {fontFamily: "Times New Roman", fontSize: "52px", color: "#FF0000"}).setOrigin(0.5);
 
+        this.add.text(centerX, 350, "Press Space to Continue", {fontFamily: "Times New Roman", fontSize: "32px", color: "#FF0000"}).setOrigin(0.5);
+
         let cursors= this.input.keyboard.createCursorKeys();
         let space= this.input.keyboard.addKey("SPACE");
         space.on('down', () => {
