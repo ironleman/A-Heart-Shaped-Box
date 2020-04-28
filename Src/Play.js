f78e5e3f40a16961ba2b9c1d69f92a3f403e3c22
ironleman <40055205+ironleman@users.noreply.github.com>
2020-04-28 12:02:36 -0700

    Heart movement + resetting

--- Src/Play.js
+++ Src/Play.js
@@ -29,17 +29,19 @@ class Play extends Phaser.Scene {
 
         this.jude = this.physics.add.sprite(game.config.width/2, game.config.height/2, 'jude_atlas', 'Jude1');
 
+        this.obstacle = this.add.sprite(game.config.width, game.config.height/2 + 100, 'heart');
+
         this.instructions= this.add.text(centerX, 150, "Press Space to Jump", {fontFamily: "Times New Roman", fontSize: "32px", color: "#FF0000"}).setOrigin(0.5);
 
         
-        this.anims.create({ 
+        this.run = this.anims.create({ 
             key: 'run', 
             frames: this.anims.generateFrameNames('jude_atlas', {      
                 prefix: 'Jude',
                 start: 1,
                 end: 3,
-                suffix: '.png',
-                //zeroPad: 4 
+                suffix: '',
+                zeroPad: 4 
             }), 
             frameRate: 30,
             repeat: -1 
@@ -57,12 +59,21 @@ class Play extends Phaser.Scene {
         this.road.tilePositionX +=6;
 
         //to do: update animation so that it plays every frame
-        
+        //this.run.anims.play('run', true);
 
         this.delayText.setText('Time: ' + Math.floor(this.clock.getElapsedSeconds().toString().substr(0, 4)) + "s");
 
+
+
+        // heart movement / resetting
+        this.num = Math.floor(Math.random() * Math.floor(150));
+
+        this.obstacle.x -= 6;
+
+        if (this.obstacle.x <= 0) {
+            if(this.num == 10){
+                this.obstacle.x = game.config.width;
+           }
+        }
     }
-
-
-    
 }
